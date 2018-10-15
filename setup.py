872dfde926908ac3db1a562336bf8fb388fde48d
NickleDave <nickledave@users.noreply.github.com>
2018-10-15 12:39:35 -0400

    add setup.py, and *egg-info to .gitignore

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='RAM',
+    version='0.1a',
+    description="""Tensorflow implementation of Recurrent Models of Visual Attention: 
+    Mnih, Volodymyr, Nicolas Heess, and Alex Graves.
+    "Recurrent models of visual attention."
+    Advances in neural information processing systems. 2014.https://arxiv.org/abs/1406.6247""",
+    author='David Nicholson',
+    author_email='https://github.com/NickleDave/RAM/issues',
+    url='https://github.com/NickleDave/RAM',
+    packages=find_packages(),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+    ]
+    )
