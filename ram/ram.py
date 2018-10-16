bd8fdf91d584af1e5926589a1ba24dfa7ee55073
NickleDave <nickledave@users.noreply.github.com>
2018-10-15 21:27:53 -0400

    add reset method to RAM model

--- ram/ram.py
+++ ram/ram.py
@@ -31,8 +31,6 @@ class RAM:
        https://arxiv.org/abs/1406.6247
     """
 
-    Out = namedtuple('out', ['h_t', 'l_t', 'b_t', 'log_probas', 'log_pi'])
-
     def __init__(self,
                  g_w=8,
                  k=3,
@@ -42,6 +40,7 @@ class RAM:
                  g_size=256,
                  hidden_size=256,
                  glimpses=6,
+                 batch_size=10,
                  num_classes=10,
                  loc_std=0.1
                  ):
@@ -73,6 +72,8 @@ class RAM:
             Size of hidden layer (number of units) in CoreNetwork
         glimpses : int
             Number of glimpses to take before acting.
+        batch_size : int
+            Number of samples per batch. Default is 10.
         num_classes : int
             Number of classes, i.e. for MNIST, num_classes = 10
         loc_std : float
@@ -90,6 +91,7 @@ class RAM:
         self.g_size = g_size
         self.hidden_size = hidden_size
         self.glimpses = glimpses
+        self.batch_size = batch_size
         self.num_classes = num_classes
         self.loc_std = loc_std
 
@@ -100,7 +102,27 @@ class RAM:
         self.action_network = modules.ActionNetwork(num_classes)
         self.baseline = modules.BaselineNetwork()
 
-        self.Out = namedtuple('Out', ['h_t', 'mu', 'l_t', 'log_pi', 'log_probas'])
+        self.Out = namedtuple('Out', ['h_t', 'b_t', 'l_t', 'mu', 'log_pi', 'log_probas'])
+
+        self.initial_l_t = tf.distributions.Uniform(low=-1.0, high=1.0)
+
+    def reset(self):
+        """get initial states for h_t and l_t.
+        called at the beginning of an episode / trial.
+
+        Returns
+        -------
+        h_t : tf.Tensor
+            of zeros
+        l_t : tf.Tensor
+            with size (self.batch_size, 2),
+            drawn from uniform distribution between -1 and 1.
+        """
+        # TODO: test other initializations for hidden state besides zeros
+        # see https://r2rt.com/non-zero-initial-states-for-recurrent-neural-networks.html
+        h_t = tf.zeros(shape=(self.batch_size, self.hidden_size,))
+        l_t = self.initial_l_t.sample(sample_shape=(self.batch_size, 2))
+        return h_t, l_t
 
     def step(self, images, l_t_minus_1, h_t_minus_1):
         """executes one time step of the model.
@@ -125,11 +147,12 @@ class RAM:
         h_t = self.core_network.forward(g_t, h_t_minus_1)
         mu, l_t = self.location_network.forward(h_t)
         b_t = self.baseline(h_t)
+
         log_pi = tf.distributions.Normal(
             loc=mu, scale=self.loc_std).log_prob(value=l_t)
         log_pi = tf.reduce_sum(log_pi, axis=1)
         log_probas = self.action_network(h_t)
-        out = self.Out(h_t, mu, l_t, log_pi, log_probas)
+        out = self.Out(h_t, l_t, b_t, mu, log_pi, log_probas)
         return out
 
 
