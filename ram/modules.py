trainer saves examples, fix calculation of upper left corner in GlimpseSensor

ram/modules.py
@@ -8,8 +8,17 @@ https://github.com/kevinzakka/recurrent-visual-attention
    Advances in neural information processing systems. 2014.
    https://arxiv.org/abs/1406.6247
 """
+from typing import NamedTuple
 
+import numpy as np
 import tensorflow as tf
+
+
+class Glimpse(NamedTuple):
+    """represents output of GlimpseSensor"""
+    rho: tf.Tensor
+    fixations: np.ndarray
+    top_left_corners: np.ndarray
 
 
 class GlimpseSensor(tf.keras.Model):
@@ -52,11 +61,20 @@ class GlimpseSensor(tf.keras.Model):
 
         Returns
         -------
-        rho : tf.Tensor
-            with shape (B, k, g_w, g_w,
-            retina-like representation of k patches of increasing size
-            and decreasing resolution, centered around location loc within
-            image img
+        Glimpse : NamedTuple
+            typed tuple, with following fields
+                rho : tf.Tensor
+                    with shape (B, k, g_w, g_w,
+                    retina-like representation of k patches of increasing size
+                    and decreasing resolution, centered around location loc within
+                    image img
+                fixations: np.ndarray
+                    locations where glimpse sensor "fixates" converted from
+                    normalized values in loc_normd to pixels in co-ordinate plane
+                    of input images.
+                top_left_corners: np.ndarray
+                    calculated top left corners of extracted glimpse 'patches'.
+                    Useful for plotting the glimpses.
         """
         batch_size, img_H, img_W, C = images.shape.as_list()
         # convert image co-ordinates from normalized to co-ordinates within
@@ -65,9 +83,10 @@ class GlimpseSensor(tf.keras.Model):
         loc_0 = tf.cast(tf.round(loc_0), tf.int32)
         loc_1 = ((loc_normd[:, 1] + 1) / 2) * img_W
         loc_1 = tf.cast(tf.round(loc_1), tf.int32)
-        loc = tf.stack([loc_0, loc_1], axis=1)
+        fixations = tf.stack([loc_0, loc_1], axis=1)
 
         rho = []
+        top_left_corners = []
         for ind in range(batch_size):
             img = images[ind, :, :, :]
             patches = []
@@ -83,9 +102,9 @@ class GlimpseSensor(tf.keras.Model):
                                                           target_width=(size * 2) + img_W)
 
                 # compute top left corner of patch
-                patch_x = loc[ind, 0] - (size // 2) + size
-                patch_y = loc[ind, 1] - (size // 2) + size
-
+                patch_x = fixations[ind, 0] - (size // 2)
+                patch_y = fixations[ind, 1] - (size // 2)
+                top_left_corners.append(np.asarray([patch_x.numpy(), patch_y.numpy()]))
                 patch = tf.slice(img_padded,
                                  begin=tf.stack([patch_x, patch_y, 0]),
                                  size=tf.stack([size, size, C])
@@ -102,7 +121,9 @@ class GlimpseSensor(tf.keras.Model):
             rho.append(patches)
 
         rho = tf.stack(rho)
-        return rho
+        fixations = fixations.numpy()
+        top_left_corners = np.asarray(top_left_corners)
+        return Glimpse(rho, fixations, top_left_corners)
 
 
 class GlimpseNetwork(tf.keras.Model):
@@ -151,7 +172,7 @@ class GlimpseNetwork(tf.keras.Model):
         self.theta_g_2 = tf.keras.layers.Dense(units=h_gt_units, activation='relu')
 
     def forward(self, images, loc):
-        """
+        """computes forward pass through GlimpseNetwork
 
         Parameters
         ----------
@@ -164,20 +185,29 @@ class GlimpseNetwork(tf.keras.Model):
 
         Returns
         -------
-        rho : tf.Tensor
-            with shape
-            (batch size, number of glimpses, glimpse size, glimpse size, channels);
-            Glimpse representation extracted by GlimpseSensor.
+        Glimpse : NamedTuple
+            typed tuple, with following fields
+                rho : tf.Tensor
+                    with shape
+                    (batch size, number of glimpses, glimpse size, glimpse size, channels);
+                    Glimpse representation extracted by GlimpseSensor.
+            fixations: np.ndarray
+                locations where glimpse sensor "fixates" converted from
+                normalized values in loc_normd to pixels in co-ordinate plane
+                of input images.
+            top_left_corners: np.ndarray
+                calculated top left corners of extracted glimpse 'patches'.
+                Useful for plotting the glimpses.
         g_t : tf.Tensor
             glimpse representation, output by glimpse network
         """
-        rho = self.glimpse_sensor.glimpse(images, loc)
-        batch_size, k, g, _, channels = rho.shape.as_list()
-        rho_vec = tf.reshape(rho, shape=(batch_size, k * g * g * channels))
+        glimpse = self.glimpse_sensor.glimpse(images, loc)
+        batch_size, k, g, _, channels = glimpse.rho.shape.as_list()
+        rho_vec = tf.reshape(glimpse.rho, shape=(batch_size, k * g * g * channels))
         h_g = self.theta_g_0(rho_vec)
         h_l = self.theta_g_1(loc)
         g_t = self.theta_g_2(h_g + h_l)
-        return rho, g_t
+        return glimpse, g_t
 
 
 class CoreNetwork(tf.keras.Model):

ram/ram.py
@@ -10,10 +10,14 @@ https://github.com/seann999/tensorflow_mnist_ram
 """
 from collections import namedtuple
 
+import numpy as np
 import tensorflow as tf
 from tensorflow.python.training.checkpointable import tracking
 
 from . import modules
+
+
+StateAndMeta = namedtuple('StateAndMeta', ['rho', 'fixations', 'top_left_corners', 'h_t', 'mu', 'l_t', 'a_t', 'b_t'])
 
 
 class RAM(tracking.Checkpointable):
@@ -164,8 +168,6 @@ class RAM(tracking.Checkpointable):
         self.action_network = modules.ActionNetwork(num_classes)
         self.baseline = modules.BaselineNetwork()
 
-        self.Out = namedtuple('Out', ['rho', 'h_t', 'mu', 'l_t', 'a_t', 'b_t'])
-
         self.initial_l_t_distrib = tf.distributions.Uniform(low=-1.0, high=1.0)
 
     def reset(self):
@@ -184,7 +186,7 @@ class RAM(tracking.Checkpointable):
         # see https://r2rt.com/non-zero-initial-states-for-recurrent-neural-networks.html
         h_t = tf.zeros(shape=(self.batch_size, self.hidden_size,))
         l_t = self.initial_l_t_distrib.sample(sample_shape=(self.batch_size, 2))
-        out = self.Out(None, h_t, None, l_t, None, None)
+        out = StateAndMeta(None, None, None, h_t, None, l_t, None, None)
         return out
 
     def step(self, images, l_t_minus_1, h_t_minus_1):
@@ -203,7 +205,7 @@ class RAM(tracking.Checkpointable):
 
         Returns
         -------
-        out : self.Out
+        State : self.Out
             namedtuple with fields h_t, mu, l_t, a_t, and b_t
                 rho : tf.Tensor
                     glimpse representation extracted by glimpse sensor from image
@@ -223,13 +225,13 @@ class RAM(tracking.Checkpointable):
                     Provides estimate of q(t) that is used during
                     training to reduce variance.
         """
-        rho, g_t = self.glimpse_network.forward(images, l_t_minus_1)
+        glimpse, g_t = self.glimpse_network.forward(images, l_t_minus_1)
         h_t = self.core_network.forward(g_t, h_t_minus_1)
         mu, l_t = self.location_network.forward(h_t)
         b_t = self.baseline.forward(h_t)
         a_t = self.action_network.forward(h_t)
-        out = self.Out(rho, h_t, mu, l_t, a_t, b_t)
-        return out
+        return StateAndMeta(glimpse.rho, glimpse.fixations, glimpse.top_left_corners,
+                            h_t, mu, l_t, a_t, b_t)
 
 
 

ram/trainer.py
@@ -49,7 +49,8 @@ class Trainer:
                              f'This will cause an error when training network;'
                              f'please change either so that data.num_samples % config.train.batch_size == 0:')
         # apply model config
-        self.model = ram.RAM(**config.model._asdict())
+        self.model = ram.RAM(batch_size=config.train.batch_size,
+                             **config.model._asdict())
 
         # then unpack train config
         self.dataset = data.dataset
@@ -71,6 +72,12 @@ class Trainer:
         self.checkpointer = tf.train.Checkpoint(optimizer=self.optimizer,
                                                 model=self.model,
                                                 optimizer_step=tf.train.get_or_create_global_step())
+        if hasattr(config.train, 'save_examples_every'):
+            self.save_examples_every = config.train.save_examples_every
+            self.examples_dir = os.path.abspath(config.train.examples_dir)
+            if not os.path.isdir(self.examples_dir):
+                os.makedirs(self.examples_dir)
+            self.num_examples_to_save = config.train.num_examples_to_save
 
     def load_checkpoint(self):
         """loads model and optimizer from a checkpoint.
@@ -92,15 +99,21 @@ class Trainer:
         else:
             print('config.train.resume is False,\n'
                   f'will save new model and optimizer to checkpoint: {self.checkpoint_path}')
-        for epoch in range(self.epochs):
+        for epoch in range(1, self.epochs+1):
             print(
-                f'\nEpoch: {epoch+1}/{self.epochs} - learning rate: {self.learning_rate:.6f}'
+                f'\nEpoch: {epoch}/{self.epochs} - learning rate: {self.learning_rate:.6f}'
             )
-            mean_acc, mean_loss = self._train_one_epoch()
+
+            if epoch % self.save_examples_every == 0:
+                save_examples = True
+            else:
+                save_examples = False
+
+            mean_acc, mean_loss = self._train_one_epoch(current_epoch=epoch, save_examples=save_examples)
             print(f'mean accuracy: {mean_acc}\nmean losses: {mean_loss}')
             self.save_checkpoint()
 
-    def _train_one_epoch(self):
+    def _train_one_epoch(self, current_epoch, save_examples=False):
         """helper function that trains for one epoch.
         Called by trainer.train"""
         losses_reinforce = []
@@ -116,18 +129,24 @@ class Trainer:
 
                 out_t_minus_1 = self.model.reset()
 
-                locs = []
                 mus = []
                 log_pis = []
                 baselines = []
+                if save_examples:
+                    locs = []
+                    fixations = []
+                    top_left_corners = []
 
                 with tf.GradientTape(persistent=True) as tape:
                     for t in range(self.model.glimpses):
                         out = self.model.step(img, out_t_minus_1.l_t, out_t_minus_1.h_t)
 
-                        locs.append(out.l_t)
                         mus.append(out.mu)
                         baselines.append(out.b_t)
+                        if save_examples:
+                            locs.append(out.l_t.numpy()[:self.num_examples_to_save, :])
+                            fixations.append(out.fixations[:self.num_examples_to_save, :])
+                            top_left_corners.append(out.top_left_corners[:self.num_examples_to_save, :])
 
                         # determine probability of choosing location l_t, given
                         # distribution parameterized by mu (output of location network)
@@ -210,6 +229,26 @@ class Trainer:
                     )
                 )
                 progress_bar.update(self.batch_size)
+
+        if save_examples:
+            locs = np.asarray(locs)
+            locs.dump(os.path.join(self.examples_dir,
+                                   f'locations_epoch_{current_epoch}'))
+            fixations = np.asarray(fixations)
+            fixations.dump(os.path.join(self.examples_dir,
+                                        f'fixations_epoch_{current_epoch}'))
+            top_left_corners = np.asarray(top_left_corners)
+            top_left_corners.dump(os.path.join(self.examples_dir,
+                                   f'top_left_corners_epoch_{current_epoch}'))
+            img = img.numpy()[:self.num_examples_to_save]
+            img.dump(os.path.join(self.examples_dir,
+                                  f'images_epoch_{current_epoch}'))
+            glimpses = out.rho.numpy()[:self.num_examples_to_save]
+            glimpses.dump(os.path.join(self.examples_dir,
+                                       f'glimpses_epoch_{current_epoch}'))
+            pred = predicted.numpy()[:self.num_examples_to_save]
+            pred.dump(os.path.join(self.examples_dir,
+                                   f'predictions_epoch_{current_epoch}'))
 
         mn_acc = np.mean(accs)
 