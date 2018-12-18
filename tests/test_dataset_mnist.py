12b1b6ec16ae6e5ca5af6b5c2240e7f70fe34a56
NickleDave <nickledave@users.noreply.github.com>
2018-12-17 22:37:42 -0500

    add test_prep_with_val in tests/test_dataset_mnist.py, to test creating validation set from MNIST works

--- tests/test_dataset_mnist.py
+++ tests/test_dataset_mnist.py
@@ -37,6 +37,19 @@ class TestDatasetMNIST(tf.test.TestCase):
             for file_type, file_path in paths_mapping.items():
                 self.assertTrue(os.path.isfile(file_path))
 
+    @tfe.run_test_in_graph_and_eager_modes
+    def test_prep_with_val(self):
+        # make sure prep function works when validation set is created by specifying val_size
+        paths_dict = ram.dataset.mnist.prep(download_dir=self.tmp_output_dir, val_size=0.25,
+                                            random_seed=None, output_dir=self.tmp_output_dir)
+        self.assertTrue(list(paths_dict.keys()) == ['train', 'test', 'val'])
+        for split, paths_mapping in paths_dict.items():
+            for file_type, file_path in paths_mapping.items():
+                self.assertTrue(os.path.isfile(file_path))
+        train_inds = np.load(paths_dict['train']['sample_inds'])
+        val_inds = np.load(paths_dict['val']['sample_inds'])
+        self.assertTrue(not any([val_ind in train_inds for val_ind in val_inds]))
+
 
 if __name__ == '__main__':
     tf.test.main()
