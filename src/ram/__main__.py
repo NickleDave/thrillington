901c9c451c948d873713f0a11b929eb92771102d
NickleDave <nickledave@users.noreply.github.com>
2018-12-18 18:45:00 -0500

    add src/ram/__main__.py module

---
+++ src/ram/__main__.py
@@ -0,0 +1,50 @@
+"""
+Invokes __main__ when the module is run as a script.
+Example: python -m ram --help
+The same function is run by the script `ram-cli` which is installed on the
+path by pip, so `$ ram-cli --help` would have the same effect (i.e., no need
+to type the python -m)
+"""
+import argparse
+
+import tensorflow as tf
+
+import ram
+
+tf.enable_eager_execution()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='main script',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-c', '--config', type=str,
+                        help='run experiment with configuration defined in config.ini file\n'
+                             '$ ram-cli --config scripts/ram_configs/config_2018-12-17.ini')
+    args = parser.parse_args()
+
+    config = ram.parse_config(args.config)
+
+    data_dir = config.data.data_dir
+    if config.data.type == 'mnist':
+        if config.data.val_size:
+            paths_dict = ram.dataset.mnist.prep(download_dir=data_root,
+                                                train_size=config.data.train_size,
+                                                val_size=config.data.val_size,
+                                                output_dir=data_root)
+            train_data, val_data = ram.dataset.mnist.get_split(paths_dict, setname=['train', 'val'])
+        else:
+            paths_dict = ram.dataset.mnist.prep(download_dir=data_root,
+                                                train_size=config.data.train_size,
+                                                output_dir=data_root)
+            train_data = ram.dataset.mnist.get_split(paths_dict, setname=['train'])
+            val_data = None
+
+    for replicate in range(config.train.replicates):
+        trainer = ram.Trainer(config=config,
+                              train_data=train_data,
+                              val_data=val_data)
+        trainer.train()
+
+
+if __name__ == '__main__':
+    main()
