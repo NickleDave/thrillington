b80adc55f6ce1ece5f533ceb7aabc57dc0359cc2
NickleDave <nickledave@users.noreply.github.com>
2018-12-18 21:37:35 -0500

    rewrite tests/test_config to test new way of getting config, make sure errors catch what they should

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,45 @@
 import os
+import unittest
 
 import ram.config
 
-this_file_dir = os.path.dirname(__file__)
-default_config_file = os.path.join(this_file_dir,
-                                   '../ram/default.ini')
+HERE = os.path.dirname(__file__)
 
 
-class TestParseConfig:
-    def test_default_config(self):
-        default_config = src.ram.utils.parse_config(config_file=None)
-        default_config_from_file = src.ram.utils.parse_config(config_file=default_config_file)
-        assert default_config == default_config_from_file
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        self.test_configs_dir = os.path.join(HERE, 'test_configs')
+
+    def test_good_config(self):
+        config_file = os.path.join(self.test_configs_dir, 'good_config.ini')
+        good_config = ram.config.parse_config(config_file=config_file)
+        self.assertTrue(type(good_config) == ram.config.Config)
+
+    def test_missing_data_dir_raises(self):
+        config_file = os.path.join(self.test_configs_dir, 'missing_data_dir_config.ini')
+        with self.assertRaises(ValueError):
+            ram.config.parse_config(config_file=config_file)
+
+    def test_missing_root_results_dir_raises(self):
+        config_file = os.path.join(self.test_configs_dir, 'missing_root_results_dir_config.ini')
+        with self.assertRaises(ValueError):
+            ram.config.parse_config(config_file=config_file)
+
+    def test_invalid_model_keys_raises(self):
+        config_file = os.path.join(self.test_configs_dir, 'invalid_model_keys_config.ini')
+        with self.assertRaises(ValueError):
+            ram.config.parse_config(config_file=config_file)
+
+    def test_invalid_train_keys_raises(self):
+        config_file = os.path.join(self.test_configs_dir, 'invalid_train_keys_config.ini')
+        with self.assertRaises(ValueError):
+            ram.config.parse_config(config_file=config_file)
+
+    def test_invalid_data_keys_raises(self):
+        config_file = os.path.join(self.test_configs_dir, 'invalid_data_keys_config.ini')
+        with self.assertRaises(ValueError):
+            ram.config.parse_config(config_file=config_file)
+
+
+if __name__ == '__main__':
+    unittest.main()
