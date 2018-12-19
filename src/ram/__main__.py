4113612a2da20eaf2d5accdf68fc3ceda8e3d6d8
NickleDave <nickledave@users.noreply.github.com>
2018-12-18 22:47:58 -0500

    main() loop in __main__ does dir creating and packing into config via much boilerplate

--- src/ram/__main__.py
+++ src/ram/__main__.py
@@ -5,7 +5,9 @@ The same function is run by the script `ram-cli` which is installed on the
 path by pip, so `$ ram-cli --help` would have the same effect (i.e., no need
 to type the python -m)
 """
+import os
 import argparse
+from datetime import datetime
 
 import tensorflow as tf
 
@@ -34,8 +36,42 @@ def main():
             train_data = ram.dataset.mnist.get_split(paths_dict, setname=['train'])
             val_data = None
 
-    for replicate in range(config.train.replicates):
+    timenow = datetime.now().strftime('%y%m%d_%H%M%S')
+    results_dirname = 'RAM_results_' + timenow
+    results_dir = os.path.join(config.data.root_results_dir, results_dirname)
+    if not os.path.isdir(results_dir):
+        os.makedirs(results_dir)
+
+    for replicate in range(1, config.train.replicates + 1):
         config.train.current_replicate = replicate
+
+        replicate_results_dir = os.path.join(results_dir, f'replicate_{replicate}')
+        if not os.path.isdir(results_dir):
+            os.makedirs(results_dir)
+
+        checkpoint_dir = os.path.join(replicate_results_dir, 'checkpoint')
+        if not os.path.isdir(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        config.data.checkpoint_dir = checkpoint_dir
+
+        if hasattr(config.data, 'save_examples_every'):
+            examples_dir = os.path.join(replicate_results_dir, 'examples')
+            if not os.path.isdir(examples_dir):
+                os.makedirs(examples_dir)
+            config.data.examples_dir = examples_dir
+
+        if config.data.save_loss:
+            loss_dir = os.path.join(replicate_results_dir, 'loss')
+            if not os.path.isdir(loss_dir):
+                os.makedirs(loss_dir)
+            config.data.loss_dir = loss_dir
+
+        if config.data.save_train_inds:
+            train_inds_dir = os.path.join(replicate_results_dir, 'train_inds')
+            if not os.path.isdir(train_inds_dir):
+                os.makedirs(train_inds_dir)
+            config.data.train_inds_dir = train_inds_dir
+
         trainer = ram.Trainer(config=config,
                               train_data=train_data,
                               val_data=val_data)
