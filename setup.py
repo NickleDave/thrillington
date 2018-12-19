b91ce5945ce02049c9d6fd6bfce3d91bed184d87
NickleDave <nickledave@users.noreply.github.com>
2018-12-18 19:47:39 -0500

    add attrs to REQUIRED in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ VERSION = '0.0.1a1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'numpy', 'tensorflow', 'tqdm'
+    'numpy', 'tensorflow', 'tqdm', 'attrs'
 ]
 
 # What packages are optional?
