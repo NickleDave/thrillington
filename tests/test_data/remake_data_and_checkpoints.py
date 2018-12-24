4de7d001efb921868962283e7674b2e21c113287
NickleDave <nickledave@users.noreply.github.com>
2018-12-24 14:26:32 -0500

    test_data just has single script used by Makefile, remake_data_and_checkpoints.py

--- tests/test_data/remake_data_and_checkpoints.py
+++ tests/test_data/remake_data_and_checkpoints.py
@@ -1,8 +1,6 @@
 """makes reduced size MNIST dataset we can use just to test everything runs without crashing.
 Also make some checkpoints from training on reduced MNIST dataset,
 so we can test that Tester runs with a checkpoint without crashing"""
-import tempfile
-import shutil
 from configparser import ConfigParser
 from pathlib import Path
 
@@ -12,25 +10,20 @@ HERE = Path(__file__).parent
 
 
 def main():
-
-    tmp_config_dir = Path(tempfile.mkdtemp())
     config_parser = ConfigParser()
     config_parser.read(HERE.joinpath('../test_configs/quick_run_config.ini'))
     config_parser['data']['data_dir'] = str(HERE.joinpath('mnist'))  # will save data here
     config_parser['data']['root_results_dir'] = str(HERE)  # and also save results w/checkpoints here
-    tmp_config = tmp_config_dir.joinpath('tmp_config.ini')
-    with open(tmp_config) as tmp_config_fp:
+    tmp_config = HERE.joinpath('tmp_config.ini')
+    with open(tmp_config, 'w') as tmp_config_fp:
         config_parser.write(tmp_config_fp)
     config = ram.config.parse_config(tmp_config)
-
     paths_dict = ram.dataset.mnist.prep(download_dir=config.data.data_dir,
                                         train_size=config.data.train_size,
                                         val_size=config.data.val_size)
     train_data, val_data = ram.dataset.mnist.get_split(paths_dict, ['train', 'val'])
-    trainer = ram.Trainer(config=config)
+    trainer = ram.Trainer(config=config, train_data=train_data, val_data=val_data)
     trainer.train()
-
-    shutil.rmtree(tmp_config_dir)
 
 
 if __name__ == '__main__':
