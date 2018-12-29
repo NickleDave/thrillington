f1bca9aa05e3ceda8c29a1417b37dd94698452e1
NickleDave <nickledave@users.noreply.github.com>
2018-12-28 21:03:25 -0500

    rewrite tests/test_trainer.py; now has test to run with quick_run_config.ini

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,18 +1,31 @@
 import shutil
 import tempfile
 import unittest
+from configparser import ConfigParser
+from pathlib import Path
+import importlib
 
 import ram
+import ram.__main__
+
+
+HERE = Path(__file__).resolve().parent
 
 
 class TestInit(unittest.TestCase):
     def setUp(self):
         # Create a temporary directory
-        self.test_dir = tempfile.mkdtemp()
+        self.test_data_dir = HERE.joinpath('test_data')
+        self.test_config_dir = HERE.joinpath('test_configs')
+        self.tmp_config_dir = Path(tempfile.mkdtemp())
+        self.tmp_results_dir = Path(tempfile.mkdtemp())
+        self.tmp_data_dir = Path(tempfile.mkdtemp())
 
     def tearDown(self):
-        # Remove the directory after the test
-        shutil.rmtree(self.test_dir)
+        # Remove directories after the test
+        shutil.rmtree(self.tmp_config_dir)
+        shutil.rmtree(self.tmp_results_dir)
+        shutil.rmtree(self.tmp_data_dir)
 
     def test_num_samples_batch_size_mismatch(self):
         # __init__ should raise an error if
@@ -21,6 +34,25 @@ class TestInit(unittest.TestCase):
         data = ram.mnist.dataset.train(directory=self.test_dir, num_samples=23)
         with self.assertRaises(ValueError):
             ram.Trainer(config, data)
+
+    def test_with_quick_run_config(self):
+        # this should run without failing
+        config_parser = ConfigParser()
+        config_parser.read(self.test_config_dir.joinpath('quick_run_config.ini'))
+        config_parser['data']['data_dir'] = str(self.tmp_data_dir)
+        config_parser['data']['root_results_dir'] = str(self.tmp_results_dir)
+        tmp_config = str(self.tmp_config_dir.joinpath('tmp_config.ini'))
+        with open(tmp_config, 'w') as tmp_config_fp:
+            config_parser.write(tmp_config_fp)
+        config = ram.parse_config(tmp_config)
+        dataset_module = importlib.import_module(name=config.data.module)
+        paths_dict = dataset_module.prep(download_dir=config.data.data_dir,
+                                         train_size=config.data.train_size,
+                                         val_size=config.data.val_size,
+                                         output_dir=config.data.data_dir)
+        train_data, val_data = dataset_module.get_split(paths_dict, setname=['train', 'val'])
+        trainer = ram.Trainer.from_config(config=config, train_data=train_data, val_data=val_data)
+        trainer.train(config.data.root_results_dir)
 
 
 if __name__ == '__main__':
