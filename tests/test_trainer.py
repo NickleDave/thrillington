b740868178872eb9c0b3889ae949aff04e29f3b0
NickleDave <nickledave@users.noreply.github.com>
2018-12-30 17:12:43 -0500

    fix test_num_samples_batch_size_mismatch_raises in test_trainer.py

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -12,7 +12,7 @@ import ram.__main__
 HERE = Path(__file__).resolve().parent
 
 
-class TestInit(unittest.TestCase):
+class TestTrainer(unittest.TestCase):
     def setUp(self):
         # Create a temporary directory
         self.test_data_dir = HERE.joinpath('test_data')
@@ -27,13 +27,25 @@ class TestInit(unittest.TestCase):
         shutil.rmtree(self.tmp_results_dir)
         shutil.rmtree(self.tmp_data_dir)
 
-    def test_num_samples_batch_size_mismatch(self):
+    def test_num_samples_batch_size_mismatch_raises(self):
         # __init__ should raise an error if
         # num samples % batch size != 0
-        config = ram.config.parse_config(config_file=None)  # default batch size is 10
-        data = ram.mnist.dataset.train(directory=self.test_dir, num_samples=23)
+        config_parser = ConfigParser()
+        config_parser.read(self.test_config_dir.joinpath('bad_batch_size_config.ini'))
+        config_parser['data']['data_dir'] = str(self.tmp_data_dir)
+        config_parser['data']['root_results_dir'] = str(self.tmp_results_dir)
+        tmp_config = str(self.tmp_config_dir.joinpath('tmp_config.ini'))
+        with open(tmp_config, 'w') as tmp_config_fp:
+            config_parser.write(tmp_config_fp)
+        config = ram.parse_config(tmp_config)
+        dataset_module = importlib.import_module(name=config.data.module)
+        paths_dict = dataset_module.prep(download_dir=config.data.data_dir,
+                                         train_size=config.data.train_size,
+                                         val_size=config.data.val_size,
+                                         output_dir=config.data.data_dir)
+        train_data = dataset_module.get_split(paths_dict, setname='train')
         with self.assertRaises(ValueError):
-            ram.Trainer(config, data)
+            ram.Trainer.from_config(config=config, train_data=train_data)
 
     def test_with_quick_run_config(self):
         # this should run without failing
