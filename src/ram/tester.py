acf87f9ee1a718b9c1bebd19c6ec1399eeb323d6
NickleDave <nickledave@users.noreply.github.com>
2018-12-30 20:51:43 -0500

    factor from_config out of Tester.__init__, add load_checkpoint method, fector _test_one_model out of main test method

--- src/ram/tester.py
+++ src/ram/tester.py
@@ -36,27 +36,77 @@ class Tester:
     """Class for measuring accuracy of trained RAM model on a test data set"""
     def __init__(self,
                  config,
-                 checkpoint_path
+                 batch_size,
+                 learning_rate,
+                 optimizer,
+                 test_data,
+                 replicates=1,
+                 logger=None
                  ):
         """__init__ for Trainer"""
-        self.model = ram.RAM(batch_size=config.train.batch_size,
-                             **attr.asdict(config.model))
+        self.config = config
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.optimizer = optimizer
 
-        self.learning_rate = config.train.learning_rate
+        self.save_log = save_log  # if True, will create logfile in train method, using that method's result_dir arg
+        if logger:
+            self.logger = logger
+        else:
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel('INFO')
+            self.logger.addHandler(logging.StreamHandler(sys.stdout))
+
+        self.test_data = test_data.dataset
+        self.logger.info(f'Test data: {self.test_data}')
+        self.num_test_samples = test_data.num_samples
+        self.logger.info(f'Number of samples in test data: {self.num_test_samples}')
+
+        self.replicates = replicates
+
+        # below get replaced with an object when we start training
+        self.checkpointer = None
+        self.model = None
+
+    @classmethod
+    def from_config(cls, config, test_data, logger=None):
         if config.train.optimizer == 'momentum':
-            self.optimizer = tf.train.MomentumOptimizer(momentum=config.train.momentum,
-                                                        learning_rate=self.learning_rate)
+            optimizer = tf.train.MomentumOptimizer(momentum=config.train.momentum,
+                                                   learning_rate=config.train.learning_rate)
         elif config.train.optimizer == 'sgd':
-            self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate)
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=config.train.learning_rate)
 
+        return cls(config=config,
+                   batch_size=config.train.batch_size,
+                   learning_rate=config.train.learning_rate,
+                   optimizer=optimizer,
+                   test_data=test_data,
+                   replicates=config.train.replicates,
+                   logger=logger)
+
+    def load_checkpoint(self, checkpoint_path):
+        """loads model and optimizer from a checkpoint.
+        Called when config.train.restore is True"""
+        latest_checkpoint = tf.train.latest_checkpoint(checkpoint_path)
+        if latest_checkpoint:
+            self.logger.info(f'restoring model from latest checkpoint: {latest_checkpoint}')
+            self.checkpointer.restore(latest_checkpoint)
+        else:
+            raise ValueError(f'no checkpoint found in checkpoint path: {checkpoint_path}')
+
+    def test(self, results_dir):
+        self.model = ram.RAM(batch_size=self.batch_size,
+                             **attr.asdict(self.config.model))
         checkpointer = tf.train.Checkpoint(optimizer=self.optimizer,
                                            model=self.model,
                                            optimizer_step=tf.train.get_or_create_global_step())
         checkpointer.restore(tf.train.latest_checkpoint(checkpoint_path))
+        for replicate in range(1, self.replicates + 1):
+            self.logger.info(f'replicate {replicate} of {self.replicates}')
 
-        self.batch_size = config.train.batch_size
+            self._test_one_model()
 
-    def test(self, test_data, save_examples=False, test_examples_dir='./test_examples/',
+    def _test_one_model(self, test_data, save_examples=False, test_examples_dir='./test_examples/',
              num_examples_to_save=9):
         """compute accuracy of trained RAM model on test data set"""
         accs = []
