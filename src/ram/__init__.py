ef985f34f094c1a93d29629c9023941e15aa2dac
NickleDave <nickledave@users.noreply.github.com>
2019-04-22 21:10:11 -0400

    rearrange imports in ram/__init__.py,  add import of plot and utils

--- src/ram/__init__.py
+++ src/ram/__init__.py
@@ -1,7 +1,10 @@
+from . import utils
+from . import plot
+from . import modules
+from . import dataset
+
 from .ram import RAM
 from .config import parse_config
 from .trainer import Trainer
 from .tester import Tester
-from . import modules
-from . import dataset
 from .__main__ import cli
