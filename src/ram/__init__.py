7e515225b65e1a222ff7990beeb9bc1af41b015e
NickleDave <nickledave@users.noreply.github.com>
2019-07-06 17:25:49 -0400

    ram/__init__.py imports Runner from runner

--- src/ram/__init__.py
+++ src/ram/__init__.py
@@ -3,8 +3,9 @@ from . import plot
 from . import modules
 from . import dataset
 
-from .ram import RAM
+from .__main__ import cli
 from .config import parse_config
+from .ram import RAM
+from .runner import Runner
 from .trainer import Trainer
 from .tester import Tester
-from .__main__ import cli
