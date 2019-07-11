3bec5b1547d3b4bf5f86b9ec311f811d6a8cc390
NickleDave <nickledave@users.noreply.github.com>
2019-07-10 22:51:22 -0500

    fix indentation in behavior-analysis-thrills script

--- src/scripts/behavior-analysis-thrills-run-script.py
+++ src/scripts/behavior-analysis-thrills-run-script.py
@@ -24,17 +24,17 @@ def main():
     suptitle = "learning rate = 0.001, standard devation = 0.2, test acc = 93%"
     save_as = "run_results_lr0.01_std0.2_testacc93_sample2.png"
     ram.plot.behavior(run_results, suffix='_from_run',
-             plot_same_sample_diff_eps=True,
-             which_sample=2,
-             suptitle=suptitle, save_as=save_as)
+                      plot_same_sample_diff_eps=True,
+                      which_sample=2,
+                      suptitle=suptitle, save_as=save_as)
 
     run_results = '/home/bart/Documents/data/RAM_output/RAM_results_190420_231619/run_results_190706_185402/replicate_1/examples'
     suptitle = "learning rate = 0.01, standard devation = 0.2, test acc = 73.8%"
     save_as = "run_results_lr0.001_std1.0_testacc73.8_sample2.png"
     ram.plot.behavior(run_results, suffix='_from_run',
-		      plot_same_sample_diff_eps=True,
-		      which_sample=2,
-		      suptitle=suptitle, save_as=save_as)
+                      plot_same_sample_diff_eps=True,
+                      which_sample=2,
+                      suptitle=suptitle, save_as=save_as)
 
 
 if __name__ == '__main__':
